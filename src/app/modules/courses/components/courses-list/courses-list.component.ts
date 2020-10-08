82826d4b9a4f556b6a5fb64d986d02056e923a9d
AlexG <aga@guidnew.com>
2020-10-08 12:31:07 +0300

    Added courses filtering

--- src/app/modules/courses/components/courses-list/courses-list.component.ts
+++ src/app/modules/courses/components/courses-list/courses-list.component.ts
@@ -1,22 +1,46 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { CourseDtoModel, FilterPanelModel } from '../../../../models';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {CourseDtoModel, CourseStatus, FilterPanelModel} from '../../../../models';
 
 @Component({
   selector: 'app-courses-list',
   templateUrl: './courses-list.component.html',
   styleUrls: ['./courses-list.component.scss']
 })
-export class CoursesListComponent implements OnInit {
+export class CoursesListComponent implements OnInit, OnChanges {
 
   @Input() public coursesData: CourseDtoModel[];
+
+  public filteredCourses: CourseDtoModel[];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.coursesData) {
+      this.filteredCourses = changes.coursesData.currentValue || [];
+    }
+  }
+
   public onFiltersChanged(filtersData: FilterPanelModel): void {
-    console.log(filtersData);
+    this.filteredCourses = this.filterCourses(this.coursesData, filtersData);
+  }
+
+  private filterCourses(courses: CourseDtoModel[], filtersData: FilterPanelModel): CourseDtoModel[] {
+    return courses.filter((course) => {
+      return this.isMatchedByStatus(course, filtersData.status) && this.isMatchedByText(course, filtersData.searchText);
+    });
+  }
+
+  private isMatchedByStatus(course: CourseDtoModel, status: CourseStatus | string): boolean {
+    return (!status || status === course.status);
+  }
+
+  private isMatchedByText(course: CourseDtoModel, searchText: string): boolean {
+    return !searchText
+      || (course.name.toLowerCase().indexOf(searchText) !== -1)
+      || (course.instructors.findIndex((instructor) => instructor.name.toLowerCase().indexOf(searchText) !== -1) !== -1);
   }
 
 }
